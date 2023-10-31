013980a4d3260a205c75844e35e2cc3432f520a0
Ella Jameson <ellagjameson@gmail.com>
2023-10-30 21:30:44 -0700

    Made XP rotor infinite use, added XP obelisk fluid

---
+++ kubejs/server_scripts/recipes/create_xp.js
@@ -0,0 +1,30 @@
+// Tweaks and expands recipes for Create XP conversion and creation
+
+onEvent('recipes', event => {
+    // Make Experience Rotor reusable (not consumed)
+    event.remove({
+        input: 'create_enchantment_industry:experience_rotor'
+    })
+    event.recipes.create.mixing([
+        Fluid.of('create_enchantment_industry:experience', 10),
+        'create_enchantment_industry:experience_rotor'
+    ],[
+        'create_enchantment_industry:experience_rotor',
+        Fluid.of('sophisticatedcore:xp_still', 200)
+    ])
+    event.recipes.create.mixing([
+        Fluid.of('create_enchantment_industry:experience', 10),
+        'create_enchantment_industry:experience_rotor'
+    ], [
+        'create_enchantment_industry:experience_rotor',
+        Fluid.of('mob_grinding_utils:fluid_xp', 200)
+    ])
+    // Add missing recipes
+    event.recipes.create.mixing([
+        Fluid.of('create_enchantment_industry:experience', 10),
+        'create_enchantment_industry:experience_rotor'
+    ], [
+        'create_enchantment_industry:experience_rotor',
+        Fluid.of('experienceobelisk:cognitium', 200)
+    ])
+})
