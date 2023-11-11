61fe24199c02b53a7736b1bbf0552a572895eb6d
Ella Jameson <ellagjameson@gmail.com>
2023-11-11 01:25:54 -0700

    Update create_xp.js

--- kubejs/server_scripts/recipes/create_xp.js
+++ kubejs/server_scripts/recipes/create_xp.js
@@ -5,25 +5,37 @@ onEvent('recipes', event => {
     event.remove({
         input: 'create_enchantment_industry:experience_rotor'
     })
-    event.recipes.create.mixing([
+    // Make Experience Rotor reusable (not consumed)
+    event.recipes.create.mixing(
+    [
         Fluid.of('create_enchantment_industry:experience', 10),
         'create_enchantment_industry:experience_rotor'
     ],[
         'create_enchantment_industry:experience_rotor',
         Fluid.of('sophisticatedcore:xp_still', 200)
     ])
-    event.recipes.create.mixing([
+    event.recipes.create.mixing(
+    [
         Fluid.of('create_enchantment_industry:experience', 10),
         'create_enchantment_industry:experience_rotor'
-    ], [
+    ],[
+        'create_enchantment_industry:experience_rotor',
+        Fluid.of('sophisticatedcore:xp_still', 200)
+    ])
+    event.recipes.create.mixing(
+    [
+        Fluid.of('create_enchantment_industry:experience', 10),
+        'create_enchantment_industry:experience_rotor'
+    ],[
         'create_enchantment_industry:experience_rotor',
         Fluid.of('mob_grinding_utils:fluid_xp', 200)
     ])
     // Add missing recipes
-    event.recipes.create.mixing([
+    event.recipes.create.mixing(
+    [
         Fluid.of('create_enchantment_industry:experience', 10),
         'create_enchantment_industry:experience_rotor'
-    ], [
+    ],[
         'create_enchantment_industry:experience_rotor',
         Fluid.of('experienceobelisk:cognitium', 200)
     ])
